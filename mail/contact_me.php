bae05fc1cf1a1898d9082c8eeec937382a292aed
Denis Nunes <denis_tecnosoft@hotmail.com>
2014-10-06 11:45:44 -0300

    Correção Form de Contato

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -6,7 +6,7 @@ if(empty($_POST['name'])  		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "Favor preencher todos os campos!";
 	return false;
    }
 	
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'pandawebdev@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato Blog:  $name";
 $email_body = "Nova mensagem recebida do Blog.\n\n"."Detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMensagem:\n$message";
-$headers = "From: denis_webmaster@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: denis_webmaster@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
