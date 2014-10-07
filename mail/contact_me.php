8a4609051dea16cc5d47a1180e24223ff7637dad
Denis Nunes <denis_tecnosoft@hotmail.com>
2014-10-07 08:36:17 -0300

    Teste E_mail Alternativo no Form de Contato

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'pandawebdev@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'dnunes@londra.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato Blog:  $name";
 $email_body = "Nova mensagem recebida do Blog.\n\n"."Detalhes:\n\nNome: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMensagem:\n$message";
-$headers = "From: denis_webmaster@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: pandawebdev@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
